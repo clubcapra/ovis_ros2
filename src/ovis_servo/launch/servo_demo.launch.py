85a7465f9c1eb80c0937f97ad210b877e7a1c250
SimonR99 <simonroy99@hotmail.ca>
2024-06-24 17:37:14 +0200

    code cleaning

--- src/ovis_servo/launch/servo_demo.launch.py
+++ src/ovis_servo/launch/servo_demo.launch.py
@@ -1,13 +1,10 @@
 import os
 import yaml
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import ComposableNodeContainer
 from launch_ros.descriptions import ComposableNode
-from launch.actions import ExecuteProcess, DeclareLaunchArgument
-import xacro
 from moveit_configs_utils import MoveItConfigsBuilder
 
 
@@ -34,8 +31,6 @@ def load_yaml(package_name, file_path):
 
 
 def generate_launch_description():
-
-    
     pkg_path = os.path.join(get_package_share_directory("ovis_moveit"))
     xacro_file = os.path.join(pkg_path, 'config', "ovis.urdf.xacro")
     kine_file = os.path.join(pkg_path, 'config', "kinematics.yaml")
@@ -51,71 +46,9 @@ def generate_launch_description():
     servo_yaml = load_yaml("ovis_servo", "config/ovis_simulated_config.yaml")
     servo_params = {"moveit_servo": servo_yaml}
     kine_yaml = load_yaml("ovis_moveit", "config/kinematics.yaml")
-    kine_params = {"moveit_servo": kine_yaml}
+    kine_params = {"robot_description_kinematics": kine_yaml}
     joint_limits_yaml = load_yaml("ovis_moveit", "config/joint_limits.yaml")
     joint_limits_params = {"moveit_servo": joint_limits_yaml}
-
-    # RViz
-    rviz_config_file = (
-        get_package_share_directory("ovis_moveit") + "/config/moveit.rviz"
-    )
-    rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_config_file],
-        parameters=[
-            moveit_config.robot_description,
-            moveit_config.robot_description_semantic,
-        ],
-    )
-
-    # ros2_control using FakeSystem as hardware
-    ros2_controllers_path = os.path.join(
-        get_package_share_directory("ovis_moveit"),
-        "config",
-        "ros2_controllers.yaml",
-    )
-    ros2_control_node = Node(
-        package="controller_manager",
-        executable="ros2_control_node",
-        parameters=[
-            moveit_config.robot_description,
-            str(moveit_config.package_path / "config/ros2_controllers.yaml"),
-        ],
-        output="screen",
-    )
-
-    nodes = {}
-    controller_names = moveit_config.trajectory_execution.get(
-        "moveit_simple_controller_manager", {}).get("controller_names", [])
-    for controller in controller_names + ["joint_state_broadcaster"]:
-            node_name = f"node_{controller}"
-            nodes[node_name] = Node(
-                package="controller_manager",
-                executable="spawner",
-                arguments=[controller],
-                output="screen",
-            )
-
-    # joint_state_broadcaster_spawner = Node(
-    #     package="controller_manager",
-    #     executable="spawner",
-    #     arguments=[
-    #         "joint_state_broadcaster",
-    #         "--controller-manager-timeout",
-    #         "300",
-    #         "--controller-manager",
-    #         "/controller_manager",
-    #     ],
-    # )
-
-    # arm_controller = Node(
-    #     package="controller_manager",
-    #     executable="spawner",
-    #     arguments=["joint_trajectory_controller", "-c", "/controller_manager"],
-    # )
 
     # Launch as much as possible in components
     container = ComposableNodeContainer(
@@ -124,58 +57,11 @@ def generate_launch_description():
         package="rclcpp_components",
         executable="component_container_mt",
         composable_node_descriptions=[
-            # Example of launching Servo as a node component
-            # Assuming ROS2 intraprocess communications works well, this is a more efficient way.
-            # ComposableNode(
-            #     package="moveit_servo",
-            #     plugin="moveit_servo::ServoServer",
-            #     name="servo_server",
-            #     parameters=[
-            #         servo_params,
-            #         moveit_config.robot_description,
-            #         moveit_config.robot_description_semantic,
-            #         kine_params,
-            #         joint_limits_params,
-            #     ],
-            # ),
-            ComposableNode(
-                package="moveit_servo",
-                plugin="moveit_servo::ServoServer",
-                name="servo_server",
-                parameters=[
-                    servo_params,
-                    moveit_config.robot_description,
-                    moveit_config.robot_description_semantic,
-                    kine_params,
-                    joint_limits_params,
-                ],
-                extra_arguments=[{"use_intra_process_comms": True}],
-            ),
             ComposableNode(
                 package="moveit_servo",
                 plugin="moveit_servo::JoyToServoPub",
                 name="controller_to_servo_node",
-                extra_arguments=[{"use_intra_process_comms": True}],
-            ),            
-            ComposableNode(
-                package="robot_state_publisher",
-                plugin="robot_state_publisher::RobotStatePublisher",
-                name="robot_state_publisher",
-                parameters=[moveit_config.robot_description,
-                {"use_sim_time": True}],
-                
             ),
-            ComposableNode(
-                package="tf2_ros",
-                plugin="tf2_ros::StaticTransformBroadcasterNode",
-                name="static_tf2_broadcaster",
-                parameters=[{"child_frame_id": "/ovis_link_base", "frame_id": "/base_link"}],
-            ),
-            # ComposableNode(
-            #     package="ovis_servo",
-            #     plugin="ovis_servo::JoyToServoPub",
-            #     name="controller_to_servo_node",
-            # ),
             ComposableNode(
                 package="joy",
                 plugin="joy::Joy",
@@ -200,11 +86,6 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        #rviz_node,
-        #ros2_control_node,
-        #*nodes.values(),
-        #joint_state_broadcaster_spawner,
-        #arm_controller,
         servo_node,
         container,
     ])
