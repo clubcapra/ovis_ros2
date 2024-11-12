79d054831636eaf319a9806ca873ec04159fb3c2
SimonR99 <simonroy99@hotmail.ca>
2024-11-11 21:16:42 -0500

    add conditionnal launch

--- src/ovis_bringup/launch/sim.launch.py
+++ src/ovis_bringup/launch/sim.launch.py
@@ -1,34 +1,40 @@
 import os
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.actions import ExecuteProcess, DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler
+from launch.conditions import IfCondition
 from launch.event_handlers import OnProcessExit
 from launch.substitutions import Command 
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
+from launch.substitutions import Command, LaunchConfiguration, PythonExpression
 from launch_ros.actions import Node, SetParameter
 from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
-    # Get the launch directory
+    # Declare the 'with_rove' launch argument
+    declare_with_rove_arg = DeclareLaunchArgument(
+        'with_rove',
+        default_value='false',
+        description='Set to "true" to disable gz_sim, bridge, and rviz_launch nodes.'
+    )
+
+    # Retrieve package directories
     pkg_ovis_description = get_package_share_directory('ovis_description')
     moveit_pkg_path = get_package_share_directory('ovis_moveit')
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
     pkg_ovis_servo = get_package_share_directory('ovis_servo')
     pkg_ovis_moveit = get_package_share_directory('ovis_moveit')
 
-    # Get the URDF file (robot)
+    # Define paths to URDF and world files
     urdf_path = os.path.join(moveit_pkg_path, 'config', 'ovis.urdf.xacro')
     robot_desc = ParameterValue(Command(['xacro ', urdf_path, ' hardware_type:=', "gazebo"]), value_type=str)
-
-    # Get the URDF file (world)
     world_file_name = 'worlds/base_world.world'
     world = os.path.join(pkg_ovis_description, world_file_name)
 
-
     namespace = '/ovis'
 
-    # Takes the description and joint angles as inputs and publishes
+    # Define nodes and launch descriptions
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -37,7 +43,6 @@ def generate_launch_description():
         output='both',
         parameters=[
             {'robot_description': robot_desc},
-            {"use_sim_time": True }
         ]
     )
 
@@ -51,46 +56,42 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ovis_moveit, 'launch', 'moveit_rviz.launch.py')
         ),
-        launch_arguments={
-            "use_sim_time": "true",
-        }.items(),
+        launch_arguments={"use_sim_time": "true"}.items(),
+        condition=IfCondition(PythonExpression(["'", LaunchConfiguration('with_rove'), "' == 'false'"]))
     )
 
-    # Setup to launch the simulator and Gazebo world
     gz_sim = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')
+        ),
         launch_arguments={'gz_args': "-v 4 -r " + world}.items(),
+        condition=IfCondition(PythonExpression(["'", LaunchConfiguration('with_rove'), "' == 'false'"]))
     )
 
-    # Spawn ovis (robot arm)
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        parameters=[{
+            'config_file': os.path.join(pkg_ovis_description, 'config', 'default_bridge.yaml'),
+            'qos_overrides./tf_static.publisher.durability': 'transient_local',
+            "use_sim_time": True
+        }],
+        output='screen',
+        condition=IfCondition(PythonExpression(["'", LaunchConfiguration('with_rove'), "' == 'false'"]))
+    )
+
     ovis_spawner = Node(
         package='ros_gz_sim',
-        # namespace=namespace,
         executable='create',
         arguments=['-name', 'ovis',
                    '-topic', '/ovis/robot_description',
                    '-x', '0',
                    '-y', '0',
-                   '-z', '0.1',
-                   ],
-        output='screen',
-    )
-
-    # Bridge ROS topics and Gazebo messages for establishing communication
-    bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        parameters=[{
-            'config_file': os.path.join(pkg_ovis_description, 'config',
-                                        'default_bridge.yaml'),
-            'qos_overrides./tf_static.publisher.durability': 'transient_local',
-            "use_sim_time": True,
-        }],
+                   '-z', '0.1'],
         output='screen'
     )
 
-    servo =  IncludeLaunchDescription(
+    servo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ovis_servo, 'launch', 'servo.launch.py')
         )
@@ -116,8 +117,8 @@ def generate_launch_description():
         arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "base_link"],
     )
 
-
     return LaunchDescription([
+            declare_with_rove_arg,
             SetParameter(name='use_sim_time', value=True),
             static_tf,
             RegisterEventHandler(
@@ -140,3 +141,4 @@ def generate_launch_description():
             ovis_spawner,
             servo,
             ])
+
